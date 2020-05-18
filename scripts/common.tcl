ee617979ea0188109d30fb92db3d6a895f5cb642
vkhristenko <vdkhristenko1991@gmail.com>
2020-05-18 11:54:06 +0200

    vio for tcp iperf change output probes width

--- scripts/common.tcl
+++ scripts/common.tcl
@@ -1,7 +1,7 @@
 #VIOs
 
 create_ip -name vio -vendor xilinx.com -library ip -version 3.0 -module_name vio_iperf -dir $device_ip_dir/
-set_property -dict [list CONFIG.C_NUM_PROBE_OUT {2} CONFIG.C_EN_PROBE_IN_ACTIVITY {0} CONFIG.C_NUM_PROBE_IN {0} CONFIG.Component_Name {vio_iperf}] [get_ips vio_iperf]
+set_property -dict [list CONFIG.C_NUM_PROBE_OUT {4} CONFIG.C_PROBE_OUT2_WIDTH {8} CONFIG.C_PROBE_OUT3_WIDTH {8} CONFIG.C_EN_PROBE_IN_ACTIVITY {0} CONFIG.C_NUM_PROBE_IN {0} CONFIG.Component_Name {vio_iperf}] [get_ips vio_iperf]
 generate_target {instantiation_template} [get_files $device_ip_dir/vio_iperf/vio_iperf.xci]
 
 create_ip -name vio -vendor xilinx.com -library ip -version 3.0 -module_name vio_udp_iperf_client -dir $device_ip_dir/
