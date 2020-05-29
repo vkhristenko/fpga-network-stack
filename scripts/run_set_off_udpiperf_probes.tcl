a587c02474dd0ef5686251eb9590bf87768f1b0f
Viktor Khristenko <vdkhristenko1991@gmail.com>
2020-05-29 21:04:51 +0200

    update set on/off vio probes

--- scripts/run_set_off_udpiperf_probes.tcl
+++ scripts/run_set_off_udpiperf_probes.tcl
@@ -1,4 +1,13 @@
+# list values first
 get_property OUTPUT_VALUE [get_hw_probes runUdpExperiment]
+get_property OUTPUT_VALUE [get_hw_probes pkgWordCountUdp]
+
+# set
+set_property OUTPUT_VALUE 00 [get_hw_probes pkgWordCountUdp]
+commit_hw_vio [get_hw_probes pkgWordCountUdp]
+get_property OUTPUT_VALUE [get_hw_probes pkgWordCountUdp]
+
+# set
 set_property OUTPUT_VALUE 0 [get_hw_probes runUdpExperiment]
 commit_hw_vio [get_hw_probes runUdpExperiment]
 get_property OUTPUT_VALUE [get_hw_probes runUdpExperiment]
--- scripts/run_set_on_udpiperf_probes.tcl
+++ scripts/run_set_on_udpiperf_probes.tcl
@@ -1,3 +1,13 @@
+# list values first
 get_property OUTPUT_VALUE [get_hw_probes runUdpExperiment]
+get_property OUTPUT_VALUE [get_hw_probes pkgWordCountUdp]
+
+# set
+set_property OUTPUT_VALUE ff [get_hw_probes pkgWordCountUdp]
+commit_hw_vio [get_hw_probes pkgWordCountUdp]
+get_property OUTPUT_VALUE [get_hw_probes pkgWordCountUdp]
+
+# set
 set_property OUTPUT_VALUE 1 [get_hw_probes runUdpExperiment]
 commit_hw_vio [get_hw_probes runUdpExperiment]
+get_property OUTPUT_VALUE [get_hw_probes runUdpExperiment]
