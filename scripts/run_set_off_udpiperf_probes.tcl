4beef90db2cf27fb2636598a6b9418e0ae3ce2a9
Viktor Khristenko <vdkhristenko1991@gmail.com>
2020-05-29 12:14:24 +0200

    set on/off vios

---
+++ scripts/run_set_off_udpiperf_probes.tcl
@@ -0,0 +1,4 @@
+get_property OUTPUT_VALUE [get_hw_probes runUdpExperiment]
+set_property OUTPUT_VALUE 0 [get_hw_probes runUdpExperiment]
+commit_hw_vio [get_hw_probes runUdpExperiment]
+get_property OUTPUT_VALUE [get_hw_probes runUdpExperiment]
